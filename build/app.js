36a4917b67e92a76bc652a1be33bebee14ed3e04
Philip Harrell <alightedlamp@gmail.com>
2017-11-06 08:09:31 -0600

    Update game logic, styles

--- build/app.js
+++ build/app.js
@@ -73,99 +73,124 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 // Define elements to be used in the game as global variables
-const blanksEl = document.querySelector("#blanks"),
-  hintEl = document.querySelector("#hint"),
-  hangmanEl = document.querySelector("#hangman"),
-  missesEl = document.querySelector("#misses"),
-  scoreEl = document.querySelector("#score"),
-  statusEl = document.querySelector("#status"),
-  manEl = document.querySelector("#man");
+const blanksEl = document.querySelector('#blanks'),
+  hintEl = document.querySelector('#hint'),
+  hangmanEl = document.querySelector('#hangman'),
+  missesEl = document.querySelector('#misses'),
+  scoreEl = document.querySelector('#score'),
+  statusEl = document.querySelector('#status'),
+  manEl = document.querySelector('#man');
 
-const spacer = "&nbsp;&nbsp;";
+const spacer = '&nbsp;&nbsp;';
 
 const asciiHangman = [
-  "<p>&nbsp;&nbsp;&nbsp;O</p>",
-  "<p>&nbsp;&nbsp;-",
-  "|",
-  "-</p>",
-  "<p>&nbsp;&nbsp;&nbsp;|</p>",
-  "<p>&nbsp;/ ",
-  "&nbsp;\\</p>"
+  '<p>&nbsp;&nbsp;&nbsp;O</p>',
+  '<p>&nbsp;&nbsp;-',
+  '|',
+  '-</p>',
+  '<p>&nbsp;&nbsp;&nbsp;|</p>',
+  '<p>&nbsp;/ ',
+  '&nbsp;\\</p>'
 ];
 
-// Build Hangman game as an object
 const Game = {
   // Data array containing words and hints for each word
   gameData: __WEBPACK_IMPORTED_MODULE_0__data__["a" /* gameData */],
 
   score: 0,
   totalGuesses: asciiHangman.length,
-  currentWord: "",
+  currentWord: '',
   guesses: [],
   misses: [],
   blanks: [],
 
   startRound: function() {
+    // Check if it's a new round and reset hangman accordingly
+    if (this.misses.length === 0) {
+      // Reset hangman to empty string
+      manEl.innerHTML = '';
+      missesEl.innerHTML = '';
+    }
+
     // Pick a random word from the gameData array and set value as word's object
     this.currentWord = this.gameData[
       Math.floor(Math.random() * this.gameData.length)
     ];
 
+    // Set up the current round's blank letters and add to page
     this.blanks = this.currentWord.word
-      .toLowerCase()
-      .split("")
-      .map(el => (el === " " ? spacer : "_"));
-    blanksEl.innerHTML = this.blanks.join(" ");
+      .split('')
+      .map(el => (el === ' ' ? spacer : '_'));
+    blanksEl.innerHTML = this.blanks.join(' ');
 
     this.displayHint();
   },
 
-  // Display a hint
+  // Displays `hint` property from current word object
   displayHint: function() {
     hintEl.innerHTML = this.currentWord.hint;
   },
 
   checkGuess: function(guess) {
-    statusEl.textContent = "";
-    // If the selected letter has already been chosen, display a note to the user
+    // Reset the status text
+    statusEl.textContent = '';
+
+    // Make guess uppercase to match case of dispaly
+    guess = guess.toUpperCase();
+
+    // If the selected letter has already been chosen, update status text to warn player
     if (this.guesses.indexOf(guess) !== -1) {
-      statusEl.textContent = "Already chosen, pick again";
+      statusEl.textContent = 'Already chosen, pick again';
     } else {
       this.guesses.push(guess);
 
       let i = 0;
       let found = false;
 
-      while (this.currentWord.word.toLowerCase().indexOf(guess, i) !== -1) {
+      while (this.currentWord.word.toUpperCase().indexOf(guess, i) !== -1) {
         // Replace index at blanks array with guessed letter if found in current word
-        let answer = this.currentWord.word.toLowerCase().indexOf(guess, i);
-        found = true;
+        let answer = this.currentWord.word.toUpperCase().indexOf(guess, i);
         this.blanks[answer] = guess;
-        blanksEl.innerHTML = this.blanks.join(" ");
+
+        // Update blanks element on page
+        blanksEl.innerHTML = this.blanks.join(' ');
+
+        // Set found as true to check game progress below
+        found = true;
         i++;
       }
 
-      // If letter is not found, add to misses array and output to page
       if (!found) {
-        // Add missed letter to misses array
+        // If letter is not found, add to misses array and output to page
         this.misses.push(guess);
-        missesEl.innerHTML = this.misses.join(" ");
-        manEl.innerHTML = asciiHangman.slice(0, this.misses.length).join("");
 
-        // If total number of misses is equals number of total changes, end the game
+        // Update missed letters section
+        missesEl.innerHTML = this.misses.join(' ');
+
+        // Update hangman image
+        manEl.innerHTML = asciiHangman.slice(0, this.misses.length).join('');
+
+        // If total number of misses is equals number of total chances, the round is over
         if (this.misses.length === this.totalGuesses) {
-          statusEl.textContent = "You lose! Better luck next time.";
+          this.showLosingState();
           this.reset();
           this.startRound();
         }
+
+        // Check if blanks string is equal to current word, indicating winning round
       } else if (
-        this.blanks.join("").replace(spacer, "") ===
-        this.currentWord.word.replace(" ", "").toLowerCase()
+        this.blanks.join('').replace(spacer, '') ===
+        this.currentWord.word.replace(' ', '').toUpperCase()
       ) {
-        statusEl.textContent = "You win this round!";
+        this.showWinningState();
         this.incrementScore();
       }
     }
+  },
+
+  // Utility functions for moving game progress forward
+  showWinningState: function() {
+    statusEl.textContent = 'You won! Here is another.';
   },
   incrementScore: function(score) {
     this.score++;
@@ -173,21 +198,23 @@ const Game = {
     this.reset();
     this.startRound();
   },
+  showLosingState: function() {
+    statusEl.textContent = 'You lose! Better luck next time.';
+  },
   reset: function() {
-    this.currentWord = "";
+    this.currentWord = '';
     this.guesses = [];
     this.misses = [];
     this.blanks = [];
-
-    manEl.innerHTML = "";
   }
 };
 
+// Listen for keyboard events and validate input
 document.onkeyup = e => {
   if (e.keyCode >= 65 && e.keyCode <= 90) {
     Game.checkGuess(e.key);
   } else {
-    statusEl.textContent = "Choose a letter, please!";
+    statusEl.textContent = 'Choose a letter, please!';
   }
 };
 
@@ -203,36 +230,89 @@ window.onload = () => {
 
 "use strict";
 const gameData = [
-    {
-      word: "Eleven",
-      hint: "Person"
-    },
-    {
-      word: "Shadow Monster",
-      hint: "Thing"
-    },
-    {
-      word: "Cheif Hopper",
-      hint: "Person"
-    },
-    {
-      word: "Hawkins Lab",
-      hint: "Place"
-    },
-    {
-      word: "The Upside Down",
-      hint: "Place"
-    },
-    {
-      word: "Demogorgon",
-      hint: "Thing"
-    },
-    {
-      word: "Jonathan Byers",
-      hint: "Person"
-    }
-  ]
+  {
+    word: 'Eleven',
+    hint: 'Person'
+  },
+  {
+    word: 'Shadow Monster',
+    hint: 'Thing'
+  },
+  {
+    word: 'Cheif Jim Hopper',
+    hint: 'Person'
+  },
+  {
+    word: 'Hawkins Lab',
+    hint: 'Place'
+  },
+  {
+    word: 'The Upside Down',
+    hint: 'Place'
+  },
+  {
+    word: 'Demogorgon',
+    hint: 'Thing'
+  },
+  {
+    word: 'Jonathan Byers',
+    hint: 'Person'
+  },
+  {
+    word: 'Mike Wheeler',
+    hint: 'Person'
+  },
+  {
+    word: 'Dustin Henderson',
+    hint: 'Person'
+  },
+  {
+    word: 'Lucas Sinclair',
+    hint: 'Person'
+  },
+  {
+    word: 'Will Byers',
+    hint: 'Person'
+  },
+  {
+    word: 'Christmas Lights',
+    hint: 'Thing'
+  },
+  {
+    word: 'Snow Ball',
+    hint: 'Event'
+  },
+  {
+    word: 'Walkie-Talkie',
+    hint: 'Thing'
+  },
+  {
+    word: 'Bicycle',
+    hint: 'Thing'
+  },
+  {
+    word: 'Ghostbusters',
+    hint: 'Thing'
+  },
+  {
+    word: 'Castle Byers',
+    hint: 'Place'
+  },
+  {
+    word: 'Dart',
+    hint: 'Thing'
+  },
+  {
+    word: 'Should I Stay Or Should I Go',
+    hint: 'Soundtrack'
+  },
+  {
+    word: 'Survive',
+    hint: 'Soundtrack'
+  }
+];
 /* harmony export (immutable) */ __webpack_exports__["a"] = gameData;
+
 
 
 /***/ })
--- src/data.js
+++ src/data.js
@@ -1,30 +1,82 @@
 export const gameData = [
-    {
-      word: "Eleven",
-      hint: "Person"
-    },
-    {
-      word: "Shadow Monster",
-      hint: "Thing"
-    },
-    {
-      word: "Cheif Hopper",
-      hint: "Person"
-    },
-    {
-      word: "Hawkins Lab",
-      hint: "Place"
-    },
-    {
-      word: "The Upside Down",
-      hint: "Place"
-    },
-    {
-      word: "Demogorgon",
-      hint: "Thing"
-    },
-    {
-      word: "Jonathan Byers",
-      hint: "Person"
-    }
-  ]
+  {
+    word: 'Eleven',
+    hint: 'Person'
+  },
+  {
+    word: 'Shadow Monster',
+    hint: 'Thing'
+  },
+  {
+    word: 'Cheif Jim Hopper',
+    hint: 'Person'
+  },
+  {
+    word: 'Hawkins Lab',
+    hint: 'Place'
+  },
+  {
+    word: 'The Upside Down',
+    hint: 'Place'
+  },
+  {
+    word: 'Demogorgon',
+    hint: 'Thing'
+  },
+  {
+    word: 'Jonathan Byers',
+    hint: 'Person'
+  },
+  {
+    word: 'Mike Wheeler',
+    hint: 'Person'
+  },
+  {
+    word: 'Dustin Henderson',
+    hint: 'Person'
+  },
+  {
+    word: 'Lucas Sinclair',
+    hint: 'Person'
+  },
+  {
+    word: 'Will Byers',
+    hint: 'Person'
+  },
+  {
+    word: 'Christmas Lights',
+    hint: 'Thing'
+  },
+  {
+    word: 'Snow Ball',
+    hint: 'Event'
+  },
+  {
+    word: 'Walkie-Talkie',
+    hint: 'Thing'
+  },
+  {
+    word: 'Bicycle',
+    hint: 'Thing'
+  },
+  {
+    word: 'Ghostbusters',
+    hint: 'Thing'
+  },
+  {
+    word: 'Castle Byers',
+    hint: 'Place'
+  },
+  {
+    word: 'Dart',
+    hint: 'Thing'
+  },
+  {
+    word: 'Should I Stay Or Should I Go',
+    hint: 'Soundtrack'
+  },
+  {
+    word: 'Survive',
+    hint: 'Soundtrack'
+  }
+];
