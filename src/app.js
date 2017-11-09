5b826b1c0ddbdb08d3eaf6c8fafe5aa52c77ba8e
Philip Harrell <alightedlamp@gmail.com>
2017-11-08 21:03:16 -0600

    Update paths... again

--- src/app.js
+++ src/app.js
@@ -24,12 +24,12 @@ const preloadImages = () => {
     });
   }
   preload(
-    './img/miss1.png',
-    './img/miss2.png',
-    './img/miss3.png',
-    './img/miss4.png',
-    './img/miss5.png',
-    './img/miss6.png'
+    'img/miss1.png',
+    'img/miss2.png',
+    'img/miss3.png',
+    'img/miss4.png',
+    'img/miss5.png',
+    'img/miss6.png'
   );
 };
 
--- src/js/hangman.js
+++ src/js/hangman.js
@@ -1,8 +1,8 @@
 export const hangman = [
-  './img/miss1.png',
-  './img/miss2.png',
-  './img/miss3.png',
-  './img/miss4.png',
-  './img/miss5.png',
-  './img/miss6.png'
+  'img/miss1.png',
+  'img/miss2.png',
+  'img/miss3.png',
+  'img/miss4.png',
+  'img/miss5.png',
+  'img/miss6.png'
 ];
