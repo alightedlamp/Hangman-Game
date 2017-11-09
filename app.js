cc1765b0a761131bad61173a782b53d2645cdef4
Philip Harrell <alightedlamp@gmail.com>
2017-11-08 21:03:16 -0600

    Update paths... again

--- app.js
+++ app.js
@@ -97,12 +97,12 @@ const preloadImages = () => {
     });
   }
   preload(
-    './img/miss1.png',
-    './img/miss2.png',
-    './img/miss3.png',
-    './img/miss4.png',
-    './img/miss5.png',
-    './img/miss6.png'
+    'img/miss1.png',
+    'img/miss2.png',
+    'img/miss3.png',
+    'img/miss4.png',
+    'img/miss5.png',
+    'img/miss6.png'
   );
 };
 
@@ -373,12 +373,12 @@ const gameData = [
 
 "use strict";
 const hangman = [
-  '../img/miss1.png',
-  '../img/miss2.png',
-  '../img/miss3.png',
-  '../img/miss4.png',
-  '../img/miss5.png',
-  '../img/miss6.png'
+  'img/miss1.png',
+  'img/miss2.png',
+  'img/miss3.png',
+  'img/miss4.png',
+  'img/miss5.png',
+  'img/miss6.png'
 ];
 /* harmony export (immutable) */ __webpack_exports__["a"] = hangman;
 
